81665e48310fe6037337392b660507e1dcbed577
Jwe0 <joshuawebb2007@proton.me>
2025-04-23 15:06:40 +0100

    Allows multiple phone numbers

--- core/modules/phone/basic.py
+++ core/modules/phone/basic.py
@@ -1,11 +1,13 @@
 import json
 
 def phone_basic(self, session, phone):
-    num = phone.removeprefix("+")
-    data = json.load(open("core/dependencies/carriers.json", "r"))
-    for i in range(len(num)):
-        car = data.get(num[:i])
-        if car:
-            return {"carrier": car}
-        continue
-    return {"carrier": "Unknown"}
+    report = {}
+    numbers = phone.split(",")
+    for number in numbers:
+        num = phone.removeprefix("+")
+        data = json.load(open("core/dependencies/carriers.json", "r"))
+        for i in range(len(num)):
+            car = data.get(num[:i])
+            if car:
+                report[number] = car
+    return report
--- core/modules/phone/codeinfo.py
+++ core/modules/phone/codeinfo.py
@@ -1,17 +1,21 @@
 import json
 
 def codeinfo(self, session, number):
-    number = number.strip().replace("(", "").replace(")", "").replace("-", "").replace(" ", "")
-    def search(dump, code):
-        return dump.get(code)
-    with open("core/dependencies/callingcodes.json", "r") as f:
-        codes = json.load(f)
-    number = number.removeprefix("+")
-    info   = search(codes, number[:3])
-    if not info:
-        info = search(codes, number[:2])
-    if not info:
-        info = search(codes, number[:1])
-    if not info:
-        return ""
-    return info
+    report = {}
+    numbers = number.split(",")
+    for number in numbers:
+        number = number.strip().replace("(", "").replace(")", "").replace("-", "").replace(" ", "")
+        def search(dump, code):
+            return dump.get(code)
+        with open("core/dependencies/callingcodes.json", "r") as f:
+            codes = json.load(f)
+        number = number.removeprefix("+")
+        info   = search(codes, number[:3])
+        if not info:
+            info = search(codes, number[:2])
+        if not info:
+            info = search(codes, number[:1])
+        if not info:
+            info = {"country": "Unknown"}
+        report[number] = info
+    return report
